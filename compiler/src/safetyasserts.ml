a7d2c9765a23d727786c8a9bdeb0fe57160d1f8b
fbarros86 <xiquita.barros@gmail.com>
2025-02-24 14:00:48 +0000

    Safety checks for initialization

--- compiler/src/safetyasserts.ml
+++ compiler/src/safetyasserts.ml
@@ -1,12 +1,53 @@
 open Prog
+module E = Expr
 
+module StringSet = Set.Make(String)
+
+
+let valid_arr_abstract_pred = {
+  E.pa_name = "valid_arr";
+  E.pa_tyin = [Type.Coq_sint; Type.Coq_sword (U64); Type.Coq_sint];
+  E.pa_tyout = Type.Coq_sbool; 
+} 
+
+let valid_mem_abstract_pred = {
+  E.pa_name = "valid_mem";
+  E.pa_tyin = [ Type.Coq_sword (U64); Type.Coq_sword (U64); Type.Coq_sint];
+  E.pa_tyout = Type.Coq_sbool; 
+}
+
+let aligned_arr_abstract_pred = {
+  E.pa_name = "aligned_arr";
+  E.pa_tyin = [Type.Coq_sint;Type.Coq_sint];
+  E.pa_tyout = Type.Coq_sbool; 
+}
+
+let aligned_mem_abstract_pred = {
+  E.pa_name = "aligned_mem";
+  E.pa_tyin = [Type.Coq_sint; Type.Coq_sword (U64); Type.Coq_sint];
+  E.pa_tyout = Type.Coq_sbool; 
+}
+
+let init_var_abstract_pred = {
+  E.pa_name = "init";
+  E.pa_tyin = [Type.Coq_sbool];
+  E.pa_tyout = Type.Coq_sbool; 
+}
+
+let init_arr_abstract_pred = {
+  E.pa_name = "init_arr";
+  E.pa_tyin = [Type.Coq_sbool; Type.Coq_sint; Type.Coq_sint];
+  E.pa_tyout = Type.Coq_sbool; 
+}
 
 type safety_assert = 
  | Not_zero of expr
  | Valid_arr of var_i * expr * int
  | Valid_mem of var_i * expr * int
  | Aligned_arr  of int * expr        
- | Aligned_mem  of int * var_i * expr               
+ | Aligned_mem  of int * var_i * expr
+ | Init of var_i             
+ | Init_arr of var_i * expr * int            
 
 let get_array_size (arr: var_i): int =
   match (L.unloc arr).v_ty with
@@ -23,48 +64,47 @@ let create_assert (assertion_expr: expr): ('info, 'asm) instr =
 
 
 let safety_assert_to_cassert( a:safety_assert): ('info, 'asm) instr =
-  match a with
-    | Not_zero (expr) ->
-      let dif_zero_expr = Papp2 (E.Oneq E.Op_int, expr, Pconst Z.zero) in
-      create_assert(dif_zero_expr)
-    | Valid_arr (arr,offset,size) ->
-      let op_valid_arr = {
-        name = "valid_arr";
-        tyin = [Bty Int; Bty (U U64); Bty Int];
-        tyout = Bty Bool; 
-      } in
-      let arr_size = Pconst (Z.of_int(get_array_size arr)) in
-      let var_size = Pconst (Z.of_int((size))) in
+  let assert_expr = 
+    match a with
+      | Not_zero (expr) ->
+        Papp2 (E.Oneq E.Op_int, expr, Pconst Z.zero)
+      | Valid_arr (arr,offset,size) ->
+        let arr_size = Pconst (Z.of_int(get_array_size arr)) in
+        let var_size = Pconst (Z.of_int((size))) in
+        PappN(Oabstract valid_arr_abstract_pred,[arr_size;offset;var_size])
+      | Valid_mem (ptr,offset,size) ->
+        let var_size = Pconst (Z.of_int((size))) in
+        let ptr_var = {
+          gv  = ptr;
+          gs  = E.Slocal;
+        } in
+        PappN(Oabstract valid_mem_abstract_pred,[Pvar(ptr_var);offset;var_size]) 
+      | Aligned_arr (size,expr) ->
+        let var_size = Pconst (Z.of_int((size))) in
+        PappN(Oabstract aligned_arr_abstract_pred,[var_size;expr])
+      | Aligned_mem (size,ptr,expr) ->
+        let var_size = Pconst (Z.of_int((size))) in
+        let ptr_var = {
+          gv  = ptr;
+          gs  = E.Slocal;
+        } in
+        PappN(Oabstract aligned_mem_abstract_pred,[var_size;Pvar(ptr_var);expr])
+      | Init (var) ->
+        let var = {
+          gv  = var;
+          gs  = E.Slocal;
+        } in
+        PappN(Oabstract init_var_abstract_pred,[Pvar(var)])
+      | Init_arr (var,expr,size) ->
+        let var = {
+          gv  = var;
+          gs  = E.Slocal;
+        } in
+        let size = Pconst (Z.of_int((size))) in
+        PappN(Oabstract init_arr_abstract_pred,[Pvar(var);expr;size])
+  in
+  create_assert(assert_expr)
 
-      let valid_arr_expr = Pabstract(op_valid_arr,[arr_size;offset;var_size]) in
-      create_assert(valid_arr_expr)
-    | Valid_mem (ptr,offset,size) ->
-      let op_valid_mem = {
-        name = "valid_mem";
-        tyin = [ Bty (U U64); Bty (U U64); Bty Int];
-        tyout = Bty Bool; 
-      } in
-      let var_size = Pconst (Z.of_int((size))) in
-      let valid_mem_expr = Pabstract(op_valid_mem,[Pfvar(ptr);offset;var_size]) in
-      create_assert(valid_mem_expr)
-    | Aligned_arr (size,expr) ->
-      let op_aligned_mem = {
-        name = "aligned_arr";
-        tyin = [Bty Int;Bty Int];
-        tyout = Bty Bool; 
-      } in
-      let var_size = Pconst (Z.of_int((size))) in
-      let aligned_arr_expr = Pabstract(op_aligned_mem,[var_size;expr]) in
-      create_assert(aligned_arr_expr)
-    | Aligned_mem (size,ptr,expr) ->
-      let op_aligned_mem = {
-        name = "aligned_mem";
-        tyin = [Bty Int; Bty (U U64); Bty Int];
-        tyout = Bty Bool; 
-      } in
-      let var_size = Pconst (Z.of_int((size))) in
-      let aligned_mem_expr = Pabstract(op_aligned_mem,[var_size;Pfvar(ptr);expr]) in
-      create_assert(aligned_mem_expr)
 
 
 
@@ -82,7 +122,7 @@ let create_assert_subarray (access: Warray_.arr_access) (wsize:Wsize.wsize) (siz
     let sub_arr_size = 
       match access with
       | AAdirect -> size
-      | AAscale -> (size_of_ws wsize) * size (*TEST*)
+      | AAscale -> (size_of_ws wsize) * size 
     in
     let index = 
       match access with
@@ -108,111 +148,124 @@ let create_assert_alignment_mem (wsize:Wsize.wsize) (ptr: var_i) (e:expr): safet
   let offset = Papp2 (E.Omul E.Op_int, e, Pconst (Z.of_int(size_of_ws wsize))) in
   Aligned_mem(size_of_ws wsize,ptr,offset)
 
-let rec get_asserts_expr (e: expr) : safety_assert list =
+let rec get_asserts_expr (e: expr) (init_vars: StringSet.t) : safety_assert list =
   match e with
-  | Pconst _ -> []
-  | Pbool _ -> []
-  | Parr_init _ -> []
-  | Pvar _ -> []
+  | Pconst _ | Pbool _ | Parr_init _ -> []
+  | Pvar v ->
+      let var_name = (L.unloc v.gv).v_name in
+      if StringSet.mem var_name init_vars then [] else [Init v.gv]
   | Pget (align,a,size,var,e1) -> 
-    let oob_assert = create_assert_access_array a size var.gv e1 in
-    (match align with
-      | Aligned -> 
-        let alignment_assert = create_assert_alignment_array a size e1 in
-        [oob_assert;alignment_assert] @ get_asserts_expr e1
-      | Unaligned -> [oob_assert] @ get_asserts_expr e1
-    )    
-  | Psub  (access, wsize,size,arr, e1) -> 
-    let safety_assert = create_assert_subarray access wsize size arr.gv e1 in
-    [safety_assert] @ get_asserts_expr e1
+      let oob_assert = create_assert_access_array a size var.gv e1 in
+      let arr_name = (L.unloc var.gv).v_name in
+      let init_assert = if StringSet.mem arr_name init_vars then [] else [Init_arr (var.gv, e1, size_of_ws size)] in
+      oob_assert :: init_assert @ get_asserts_expr e1 init_vars
+  | Psub (access, wsize, size, arr, e1) -> 
+      let safety_assert = create_assert_subarray access wsize size arr.gv e1 in
+      safety_assert :: get_asserts_expr e1 init_vars
   | Pload (align,size,var,e1) -> 
-    let safety_assert = create_assert_memory_access size var e1  in
-    (match align with
-      | Aligned -> 
-      let alignment_assert = create_assert_alignment_mem size var e1 in
-      [safety_assert;alignment_assert] @ get_asserts_expr e1
-      | Unaligned -> [safety_assert] @ get_asserts_expr e1
-    ) 
-  | Papp1 (_, e1) -> get_asserts_expr e1
+      let safety_assert = create_assert_memory_access size var e1 in
+      safety_assert :: get_asserts_expr e1 init_vars
+  | Papp1 (_, e1) -> get_asserts_expr e1 init_vars
   | Papp2 (E.Odiv _, e1, denom) -> 
-    let assert_div_zero = Not_zero (denom) in
-    [assert_div_zero] @  get_asserts_expr e1 @ get_asserts_expr denom
-  | Papp2 (_, e1, e2) -> get_asserts_expr e1 @ get_asserts_expr e2
-  | PappN (_, exprs) -> List.flatten (List.map (get_asserts_expr) exprs)
-  | Pif (_, e1, e2, e3) -> 
-      get_asserts_expr e1
-      @ get_asserts_expr e2
-      @ get_asserts_expr e3
+      let asserts_e1 = get_asserts_expr e1 init_vars in
+      let asserts_denom = get_asserts_expr denom init_vars in
+      Not_zero denom :: asserts_e1 @ asserts_denom
+  | Papp2 (_, e1, e2) -> 
+      let asserts_e1 = get_asserts_expr e1 init_vars in
+      let asserts_e2 = get_asserts_expr e2 init_vars in
+      asserts_e1 @ asserts_e2
+  | PappN (_, exprs) -> 
+      List.flatten(List.map (fun e -> get_asserts_expr e init_vars) exprs)
   | _ -> []
 
-let get_asserts_lval (lval: lval) : safety_assert list =
+let get_asserts_lval (lval: lval) (init_vars: StringSet.t) : (safety_assert list * StringSet.t) =
   match lval with
-  | Lnone _ -> []
-  | Lvar _ -> []
-  | Lmem (align,size,var,e1) -> 
+  | Lnone _ -> ([], init_vars)
+  | Lvar var ->
+    let var_name = (L.unloc var).v_name in
+    let new_init_vars = StringSet.add var_name init_vars in
+    ([], new_init_vars)
+  | Lmem (align, size, var, e1) -> 
     let safety_assert = create_assert_memory_access size var e1 in
+    let asserts = get_asserts_expr e1 init_vars in
     (match align with
       | Aligned -> 
         let alignment_assert = create_assert_alignment_mem size var e1 in
-        [safety_assert;alignment_assert] @ get_asserts_expr e1
-      | Unaligned -> [safety_assert] @ get_asserts_expr e1
+        ([safety_assert;alignment_assert] @ asserts, init_vars)
+      | Unaligned -> ([safety_assert] @ asserts,init_vars)
     )
   | Laset (align,a,wsize,arr,e1) -> 
     let oob_assert = create_assert_access_array a wsize arr e1 in
+     let asserts = get_asserts_expr e1 init_vars in
     (match align with
       | Aligned -> 
         let alignment_assert = create_assert_alignment_array a wsize e1 in
-        [oob_assert;alignment_assert] @ get_asserts_expr e1
-      | Unaligned -> [oob_assert] @ get_asserts_expr e1
+        ([oob_assert;alignment_assert] @ asserts,init_vars)
+      | Unaligned -> ([oob_assert] @ asserts,init_vars)
     )
-  | Lasub (a,wsize,size,arr,e1) -> [create_assert_subarray a wsize size arr e1] @ get_asserts_expr e1
+  | Lasub (a,wsize,size,arr,e1) ->
+     let asserts = get_asserts_expr e1 init_vars in
+    ([create_assert_subarray a wsize size arr e1] @ asserts, init_vars)
 
-let rec add_asserts_function (instructions: (int, 'info, 'asm) ginstr list) : (int, 'info, 'asm) ginstr list =
+
+
+let rec add_asserts_function (instructions: (int, 'info, 'asm) ginstr list) (init_vars: StringSet.t) : ((int, 'info, 'asm) ginstr list * StringSet.t) =
   match instructions with
-  | [] -> []
+  | [] -> ([], init_vars)
   | i :: rest ->
-      let new_instrs =
+      let (new_instrs, new_init_vars) =
         match i.i_desc with
         | Cassgn (lval, _, _, expr) -> 
-            let asserts_lval = get_asserts_lval (lval) in
-            let asserts_expr = get_asserts_expr expr in
+            let (asserts_lval, init_vars1) = get_asserts_lval lval init_vars in
+            let asserts_expr = get_asserts_expr expr init_vars in
             let asserts = List.map safety_assert_to_cassert (asserts_lval @ asserts_expr) in
-            asserts @ [i]
+            (asserts @ [i], init_vars1)
         | Copn (lvals,_,_,exprs)
         | Csyscall (lvals,_,exprs)
         | Ccall (lvals,_,exprs)  -> 
-            let asserts_lvals = List.flatten (List.map get_asserts_lval lvals) in
-            let asserts_exprs = List.flatten (List.map get_asserts_expr exprs) in
+            let (asserts_lvals, init_vars1) = List.fold_left (fun (acc_asserts, acc_vars) lval ->
+              let (lval_asserts, new_vars) = get_asserts_lval lval acc_vars in
+              (acc_asserts @ lval_asserts, new_vars)
+            ) ([], init_vars) lvals in
+            let asserts_exprs = List.flatten (List.map (fun e -> get_asserts_expr e init_vars) exprs) in
             let asserts = List.map safety_assert_to_cassert (asserts_lvals @ asserts_exprs) in
-            asserts @ [i]
+            (asserts @ [i], init_vars1)
         | Cif (c,e1,e2) -> 
-            let asserts =  List.map safety_assert_to_cassert (get_asserts_expr c) in
-            let new_e1 = add_asserts_function e1 in
-            let new_e2 = add_asserts_function e2 in
-            asserts @ [ { i with i_desc = Cif (c, new_e1, new_e2) } ]
+            let asserts =  List.map safety_assert_to_cassert (get_asserts_expr c init_vars) in
+            let (new_e1,init_vars1) = add_asserts_function e1 init_vars in
+            let (new_e2,init_vars2) = add_asserts_function e2 init_vars in
+            (asserts @ [ { i with i_desc = Cif (c, new_e1, new_e2) } ], StringSet.inter init_vars1 init_vars2)
         | Cfor (x, r, body) ->
-            let new_body = add_asserts_function body in
-            let _,e1,e2 = r in
-            let asserts_exprs = get_asserts_expr e1 @ get_asserts_expr e2 in
+          let dir,e1,e2 = r in
+          if (e1<e2 && dir = Expr.DownTo) || (e1>e2 && dir = Expr.UpTo) then
+            ([i],init_vars)
+          else
+            let var_name = (L.unloc x).v_name in
+            let init_vars_for = StringSet.add var_name init_vars in
+            let new_body,new_init_vars = add_asserts_function body init_vars_for in
+            let asserts_exprs = get_asserts_expr e1 init_vars @ get_asserts_expr e2 init_vars in
             let asserts =  List.map safety_assert_to_cassert asserts_exprs in
-            asserts @ [ { i with i_desc = Cfor (x, r, new_body) } ]
+            let new_init_vars = StringSet.remove var_name new_init_vars in
+            (asserts @ [ { i with i_desc = Cfor (x, r, new_body) } ],new_init_vars)
         | Cwhile (a, body1, c, body2) ->
-            let new_body1 = add_asserts_function body1 in
-            let new_body2 = add_asserts_function body2 in
-            let asserts = List.map safety_assert_to_cassert (get_asserts_expr c) in
-            asserts @ [ { i with i_desc = Cwhile (a, new_body1, c, new_body2) } ] 
-        | _ -> [i] 
+            let new_body1,new_init_vars = add_asserts_function body1 init_vars in
+            let new_body2,_ = add_asserts_function body2 new_init_vars in
+            let asserts = List.map safety_assert_to_cassert (get_asserts_expr c new_init_vars) in
+            (asserts @ [ { i with i_desc = Cwhile (a, new_body1, c, new_body2) } ],new_init_vars)
+        | _ -> ([i], init_vars) 
       in
-      new_instrs @ add_asserts_function rest
-
+      let (rest_instrs, final_vars) = add_asserts_function rest new_init_vars in
+      (new_instrs @ rest_instrs, final_vars)
 
 let add_asserts (prog: (unit, 'asm) prog): (unit, 'asm) prog =
   match prog with
   | (globals, funcs) -> 
       let updated_funcs =
         List.map (fun f ->
-          let updated_body = add_asserts_function (f.f_body) in
+          let init_vars = List.fold_left (fun acc v -> StringSet.add v.v_name acc) StringSet.empty f.f_args in
+          let updated_body, _ = add_asserts_function f.f_body init_vars in
           { f with f_body = updated_body }
         ) funcs
       in
       (globals, updated_funcs)
+  
