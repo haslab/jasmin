b312cedb80279b2ad0e870af976c79d7be9dbe62
JBA <jba@di.uminho.pt>
2025-11-15 22:26:52 +0000

    fix abi

--- compiler/tests/success/x86-64/test_float.c
+++ compiler/tests/success/x86-64/test_float.c
@@ -3,20 +3,20 @@
 #include <stdint.h>
 
 
-extern uint64_t addsd(uint64_t x, uint64_t y);
-extern uint64_t subsd(uint64_t x, uint64_t y);
-extern uint64_t mulsd(uint64_t x, uint64_t y);
-extern uint64_t cvttsd2si(uint64_t x);
-extern uint64_t cvtsd2si(uint64_t x);
-extern uint64_t cvtsi2sd(uint64_t x);
-extern uint64_t cmpeqsd(uint64_t x, uint64_t y);
-extern uint64_t cmpltsd(uint64_t x, uint64_t y);
-extern uint64_t cmplesd(uint64_t x, uint64_t y);
-extern uint64_t cmpunordsd(uint64_t x, uint64_t y);
-extern uint64_t cmpneqsd(uint64_t x, uint64_t y);
-extern uint64_t cmpnltsd(uint64_t x, uint64_t y);
-extern uint64_t cmpnlesd(uint64_t x, uint64_t y);
-extern uint64_t cmpordsd(uint64_t x, uint64_t y);
+extern double addsd(double x, double y);
+extern double subsd(double x, double y);
+extern double mulsd(double x, double y);
+extern uint64_t cvttsd2si(double x);
+extern uint64_t cvtsd2si(double x);
+extern double cvtsi2sd(uint64_t x);
+extern uint64_t cmpeqsd(double x, double y);
+extern uint64_t cmpltsd(double x, double y);
+extern uint64_t cmplesd(double x, double y);
+extern uint64_t cmpunordsd(double x, double y);
+extern uint64_t cmpneqsd(double x, double y);
+extern uint64_t cmpnltsd(double x, double y);
+extern uint64_t cmpnlesd(double x, double y);
+extern uint64_t cmpordsd(double x, double y);
 
 
 int main() {
@@ -26,28 +26,22 @@ int main() {
  a = 1.25;
  b = 2.25;
 
- ci = addsd(*((uint64_t*)&a), *((uint64_t*)&b));
- c = *((double*)&ci);
+ c = addsd(a, b);
  printf("%lf + %lf = %lf\n", a, b, c);
- ci = subsd(*((uint64_t*)&a), *((uint64_t*)&b));
- c = *((double*)&ci);
+ c = subsd(a, b);
  printf("%lf - %lf = %lf\n", a, b, c);
- ci = mulsd(*((uint64_t*)&a), *((uint64_t*)&b));
- c = *((double*)&ci);
+ c = mulsd(a, b);
  printf("%lf * %lf = %lf\n", a, b, c);
- ci = cvttsd2si(*((uint64_t*)&c));
+ ci = cvttsd2si(c);
  printf("floor(%lf) = %lld\n", c, ci);
- ci = cvtsd2si(*((uint64_t*)&c));
+ ci = cvtsd2si(c);
  printf("round(%lf) = %lld\n", c, ci);
- ci = cmpltsd(*((uint64_t*)&a), *((uint64_t*)&b));
- c = *((double*)&ci);
- printf("%lf < %lf = %d\n", a, b, c!=0);
- ci = cmpneqsd(*((uint64_t*)&a), *((uint64_t*)&a));
- c = *((double*)&ci);
- printf("%lf != %lf = %d\n", a, a, c!=0);
+ ci = cmpltsd(a, b);
+ printf("%lf < %lf = %d\n", a, b, ci!=0);
+ ci = cmpneqsd(a, a);
+ printf("%lf != %lf = %d\n", a, a, ci!=0);
  di = 3345;
- ci = cvtsi2sd(di);
- c = *((double*)&ci);
+ c = cvtsi2sd(di);
  printf("double(%lld) = %lf\n", di, c);
 
  return 0;
